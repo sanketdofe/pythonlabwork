2d34ae6831555380995b02bb2aebb4c707518898
Sanket dofe <dofesanket@gmail.com>
2019-10-13 00:39:07 +0530

    Update profit.py

--- profit.py
+++ profit.py
@@ -17,9 +17,8 @@ print("\nID\tCost\tTime\tName\tCost/Time\n")
 for i in range(len(data1)):
 	print(*data1[i], sep ="\t")
 
-def gochi(given):
-	
 
+    
 for i in range(len(data1)):
 	for j in range(len(data1) - i-1):
 		if(data1[j][4]>data1[j+1][4]):
@@ -28,13 +27,28 @@ for i in range(len(data1)):
 				temp[k] = data1[j][k]
 				data1[j][k] = data1[j+1][k]
 				data1[j+1][k] = temp[k]
-
+data1.reverse()
 
 print("\n\n**SORTED LIST**\n")		
-print("\nID\tCost\tTime\tName\tCost/time\n")		
+print("\nID\tCost\tTime\tName\tCost/time\n")
 for i in range(len(data1)):
 	print(*data1[i], sep ="\t")
 	
 
+
+
+def gochi(given):
+    count=0
+    profit=0
+    for j in data1:
+        if j[2]<=given:
+            #temp = int(data1.index(j-1))
+            profit+= data1[count][1]
+            given-=data1[count][2]
+        count+=1
+    return profit,given
+
 x = int(input("Enter the no of hours:"))
-gochi(x)
+profit,remain=gochi(x)
+print("The max profit is " + str(profit))
+print("Remaining hours are " + str(remain))
